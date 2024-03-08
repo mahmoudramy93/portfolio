211e1e0a35856012f6a7ea5d6f0b11b9ef5c6351
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2024-03-08 18:44:01 +0200

    Adding Base Desighn

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  speed: 800,
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+// skills
+$(function () {
+  $(".chart").easyPieChart({
+    size: 100,
+    barColor: "#00c896",
+    trackColor: "#2B2B2B",
+    scaleLength: 0,
+    lineWidth: 5,
+    lineCap: "circle",
+    animate: 2000,
+  });
+});
