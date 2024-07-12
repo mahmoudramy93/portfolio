b25dcfa52792dbc4046c994d25d1eb98d877f1cd
Safeya-Yasien <bluesky7yas@gmail.com>
2024-07-12 19:59:22 +0300

    update

--- src/components/CustomDescription/CustomDescription.jsx
+++
@@ -1,7 +0,0 @@
-import "./customDescripition.scss";
-
-const CustomDescription = ({ description }) => {
-  return <p className="custom-description">{description}</p>;
-};
-
-export default CustomDescription;
--- src/components/MoreAboutMe/MoreAboutMeTitle/MoreAboutMeTitle.jsx
+++
@@ -1,7 +0,0 @@
-import "./moreAboutMeTitle.scss";
-
-const MoreAboutMeTitle = ({ title }) => {
-  return <h4 className="more-about-me-title">{title}</h4>;
-};
-
-export default MoreAboutMeTitle;
