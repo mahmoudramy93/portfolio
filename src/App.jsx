0f168a8191708c2d9e703dbf1042df4ace033249
Safeya-Yasien <bluesky7yas@gmail.com>
2024-08-01 04:08:56 +0300

    replace browser router with hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home, About, Blogs, Projects, BlogDetails } from "./pages/index";
 import { store } from "./rtk/app/store";
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
+import { HashRouter as Router } from "react-router-dom";
 import "./App.scss";
+
+import Root from "./Root";
 
 function App() {
   return (
     <Provider store={store}>
-    <BrowserRouter basename="/portfolio/">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/blogs" element={<Blogs />}></Route>
-        <Route path="/blogs/blog/:blogId" element={<BlogDetails />}></Route>
-        <Route path="/projects" element={<Projects />}></Route>
-      </Routes>
-    </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />}>
+            <Route index element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/blogs" element={<Blogs />}></Route>
+            <Route path="/blogs/blog/:blogId" element={<BlogDetails />}></Route>
+            <Route path="/projects" element={<Projects />}></Route>
+          </Route>
+        </Routes>
+      </Router>
     </Provider>
   );
 }
