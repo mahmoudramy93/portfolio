bed2db6fec66b4f5c32fb4b0e8c0aa2ddb9b6253
Safeya-Yasien <bluesky7yas@gmail.com>
2024-08-27 15:24:09 +0300

    update store

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import AppRouter from "@routes/AppRouter";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@styles/global.scss";
 import { Provider } from "react-redux";
-import { store } from "./rtk/app/store";
+import { store } from "@rtk/app/store";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
