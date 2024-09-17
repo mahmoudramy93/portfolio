55ef1391081a959ef09586ceb24202589409de68
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-18 01:25:51 +0300

    feat: :sparkles: add contact us section

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <form
+      className="space-y-8 relative"
+      action="https://api.web3forms.com/submit"
+      method="POST"
+    >
+      <div>
+        <input
+          type="hidden"
+          name="access_key"
+          defaultValue="b6cca442-88a3-4137-a8b3-38b2cc2d46cc"
+        />
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >
+          Your email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="block p-3 w-full text-sm text-gray-900 rounded-lg  shadow-sm   dark:bg-gray-700/80  dark:placeholder-white/60 dark:text-white outline-none"
+          placeholder="name@flowbite.com"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="subject"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >
+          Subject
+        </label>
+        <input
+          type="text"
+          id="subject"
+          className="block p-3 w-full text-sm text-gray-900 rounded-lg  shadow-sm   dark:bg-gray-700/80  dark:placeholder-white/60 dark:text-white outline-none"
+          placeholder="Let us know how we can help you"
+        />
+      </div>
+      <div className="sm:col-span-2">
+        <label
+          htmlFor="message"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 "
+        >
+          Your message
+        </label>
+        <textarea
+          id="message"
+          rows={6}
+          className="block p-3 w-full  text-sm text-gray-900 rounded-lg  shadow-sm   dark:bg-gray-700/80  dark:placeholder-white/60 dark:text-white outline-none resize-none"
+          placeholder="Leave a comment..."
+        />
+      </div>
+      <button
+        type="submit"
+        className="py-3 px-5 text-sm font-medium block text-center mx-auto text-tealGreen rounded-lg bg-transparent sm:w-fit border border-tealGreen transition-all duration-500 hover:bg-black hover:text-white "
+      >
+        Send message
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
---
+++ src/components/ContactUs.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { CustomTitle } from "./common";
+import ContactForm from "./ContactForm";
+
+const ContactUs = () => {
+  return (
+    <section
+      className="pt-10  px-5 bg-[url('/images/contact_us.webp')] bg-no-repeat bg-cover bg-center rounded-[50px] relative before:content[''] before:absolute before:w-full before:h-full before:left-0 before:top-0 before:bg-black/60"
+      id="contact"
+    >
+      <div className="container">
+        <CustomTitle
+          title={"Get in touch"}
+          description={"Feel free to drop me a line - Contact us"}
+        />
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <ContactForm />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactUs;
