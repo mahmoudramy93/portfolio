d91ee9c7d20c2245a4cc1dac8e0606c7d85201e1
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-18 10:06:05 +0300

    style: :lipstick:

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -15,42 +15,42 @@ const ContactForm = () => {
         />
         <label
           htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          className="block mb-2 text-sm font-medium text-gray-300"
         >
           Your email
         </label>
         <input
           type="email"
           id="email"
-          className="block p-3 w-full text-sm text-gray-900 rounded-lg  shadow-sm   dark:bg-gray-700/80  dark:placeholder-white/60 dark:text-white outline-none"
+          className="block p-3 w-full text-sm  rounded-lg  shadow-sm text-white  bg-gray-700/80  dark:placeholder-white/60  outline-none"
           placeholder="name@flowbite.com"
         />
       </div>
       <div>
         <label
           htmlFor="subject"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          className="block mb-2 text-sm font-medium text-gray-300"
         >
           Subject
         </label>
         <input
           type="text"
           id="subject"
-          className="block p-3 w-full text-sm text-gray-900 rounded-lg  shadow-sm   dark:bg-gray-700/80  dark:placeholder-white/60 dark:text-white outline-none"
+          className="block p-3 w-full text-sm  rounded-lg  shadow-sm text-white  bg-gray-700/80  dark:placeholder-white/60  outline-none"
           placeholder="Let us know how we can help you"
         />
       </div>
       <div className="sm:col-span-2">
         <label
           htmlFor="message"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 "
+          className="block mb-2 text-sm font-medium text-gray-300 "
         >
           Your message
         </label>
         <textarea
           id="message"
           rows={6}
-          className="block p-3 w-full  text-sm text-gray-900 rounded-lg  shadow-sm   dark:bg-gray-700/80  dark:placeholder-white/60 dark:text-white outline-none resize-none"
+          className="block p-3 w-full  text-sm text-white rounded-lg  shadow-sm  bg-gray-700/80  dark:placeholder-white/60 dark:text-white outline-none resize-none"
           placeholder="Leave a comment..."
         />
       </div>
