812fecba512a11a415b2f17478e8794349db50ab
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-22 16:03:26 +0300

    style: :lipstick:

--- src/data/servicesData.ts
+++ src/data/servicesData.ts
@@ -7,7 +7,6 @@ export const servicesData = [
     description:
       "I specialize in designing and developing robust backend systems that power web applications. My expertise includes crafting APIs, managing databases, and implementing business logic. I use modern frameworks like Express and Flask, combined with a deep understanding of Node.js, to build scalable and maintainable solutions that meet the specific needs of clients.",
     icon: FaServer,
-    // aosEffect: "fade-right",
   },
   {
     id: "2",
@@ -15,7 +14,6 @@ export const servicesData = [
     description:
       "I provide end-to-end DevOps services that streamline the software development lifecycle. This includes automating deployments, ensuring continuous integration, and maintaining efficient CI/CD pipelines. My goal is to enhance collaboration between development and operations, reduce deployment time, and increase the reliability of software releases.",
     icon: FaCogs,
-    // aosEffect: "fade-right",
   },
   {
     id: "3",
@@ -23,7 +21,7 @@ export const servicesData = [
     description:
       "With extensive experience in IT infrastructure, I offer services that ensure your systems are secure, scalable, and resilient. This includes network configuration, server management, and implementing security protocols. I specialize in data center security, virtualization, and cloud services, ensuring your infrastructure is both robust and compliant with industry standards.",
     icon: FaNetworkWired,
-    // aosEffect: "fade-left",
+    
   },
   {
     id: "4",
@@ -31,6 +29,6 @@ export const servicesData = [
     description:
       "I offer comprehensive system administration services that include routine maintenance, performance tuning, and automation of repetitive tasks. My focus is on maximizing system uptime, improving performance, and reducing manual intervention through scripting and automation tools like Bash, PowerShell, and Python.",
     icon: FaTools,
-    // aosEffect: "fade-left",
+    
   },
 ];
--- src/data/skillsData.ts
+++ src/data/skillsData.ts
@@ -20,43 +20,6 @@ import {
 } from "react-icons/si";
 import { FcLinux } from "react-icons/fc";
 
-// export const skills = [
-//   {
-//     category: "Software Engineering & Web Development",
-//     items: [
-//       { icon: FaPython, label: "Proficient in Python" },
-//       { icon: FaNodeJs, label: "Proficient in Node.js" },
-//       { icon: FaDatabase, label: "Backend development" },
-//       { icon: FaDatabase, label: "Database schema design" },
-//       { icon: FaServer, label: "API Design & management" },
-//     ],
-//   },
-//   {
-//     category: "System Administration & DevOps",
-//     items: [
-//       { icon: FcLinux, label: "Linux/Unix Administration" },
-//       { icon: FaWindows, label: "Windows Server Administration" },
-//       {
-//         icon: FaNetworkWired,
-//         label:
-//           "Network Configuration and Security (Cisco, Paloalto, Fortinet, Sophos)",
-//       },
-//       { icon: FaCloud, label: "Cloud Services Management (AWS, Azure)" },
-//       {
-//         icon: SiVmware,
-//         label: "Virtualization Technologies (VMware, Hyper-V)",
-//       },
-//       {
-//         icon: FaDocker,
-//         label: "Containerization & Orchestration (Docker, Kubernetes)",
-//       },
-//       {
-//         icon: FaCloudsmith,
-//         label: "Disaster Recovery and Backup Solutions (Veeam, Acronis)",
-//       },
-//     ],
-//   },
-// ];
 
 export const skills = [
   {
