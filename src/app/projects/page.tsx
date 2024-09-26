a75a11710bf8ac916b4023ad5680db0c631be9dc
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-26 10:38:30 +0300

    perf: :zap:

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,27 +4,45 @@ import { CustomTitle } from "@/components/common";
 import Project from "@/components/Project";
 import { projectsData } from "@/data/projectsData";
 import { technologiesData } from "@/data/technologiesData";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 const ProjectsPage = () => {
   const [selectedCategory, setSelectedCategory] = useState("all");
 
-  const filteredProjects =
-    selectedCategory.toLocaleLowerCase() === "all"
+  const filteredProjects = useMemo(() => {
+    return selectedCategory.toLocaleLowerCase() === "all"
       ? projectsData
       : projectsData.filter((project) =>
           project.technologies.includes(selectedCategory)
         );
+  }, [selectedCategory, projectsData]);
 
-  const renderTechnologies = technologiesData.map((tech) => (
-    <li
-      key={tech}
-      className={`cursor-pointer py-2 px-6 bg-gray-800 dark:bg-darkGray text-white dark:text-tealGreen border border-tealGreen rounded-md transition-all hover:bg-lightTealGreen hover:text-darkGray`}
-      onClick={() => setSelectedCategory(tech.toLocaleLowerCase())}
-    >
-      {tech}
-    </li>
-  ));
+  const handleCategoryChange = (tech: string) => {
+    setSelectedCategory(tech.toLocaleLowerCase());
+  };
+
+  const renderTechnologies = technologiesData.map((tech) => {
+    const isSelected =
+      selectedCategory.toLocaleLowerCase() === tech.toLocaleLowerCase();
+
+    return (
+      <li
+        key={tech}
+        className={`
+          cursor-pointer py-2 px-6 
+          bg-gray-800 dark:bg-darkGray text-white dark:text-tealGreen border border-tealGreen rounded-md transition-all 
+          hover:bg-lightTealGreen dark:hover:bg-lightTealGreen  hover:text-darkGray dark:hover:text-black
+          ${
+            isSelected
+              ? "bg-lightTealGreen dark:bg-lightTealGreen !text-darkGray"
+              : ""
+          }`}
+        onClick={() => handleCategoryChange(tech)}
+      >
+        {tech}
+      </li>
+    );
+  });
 
   const renderedProjects = filteredProjects.map((project) => (
     <Project key={project.id} {...project} />
