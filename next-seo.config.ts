69bfbb0edf7993afcdd752384dc6615dd6342bab
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-26 11:26:25 +0300

    perf: :mag: enhance seo

---
+++ next-seo.config.ts
@@ -0,0 +1,63 @@
+import { DefaultSeoProps } from "next-seo";
+
+const seoConfig: DefaultSeoProps = {
+  title: "Mahmoud Elbehery - Backend Developer",
+  description:
+    "Explore my portfolio as a backend developer, specializing in API design, database schema, and cloud deployment. Let's build something amazing together!",
+  openGraph: {
+    title: "Mahmoud Elbehery - Backend Developer",
+    description:
+      "Explore my portfolio as a backend developer, specializing in API design, database schema, and cloud deployment.",
+    url: "https://mahmoud-elbehery.netlify.app/",
+    images: [
+      {
+        url: "/images/personal_images/profile.svg",
+        width: 1200,
+        height: 630,
+        alt: "Mahmoud Elbehery Portfolio",
+      },
+      {
+        url: "/images/logo.png",
+        width: 300,
+        height: 300,
+        alt: "Mahmoud Elbehery Logo",
+      },
+    ],
+    siteName: "Mahmoud Elbehery Portfolio",
+  },
+  twitter: {
+    cardType: "summary_large_image",
+    site: "https://x.com/mahmoudramy93",
+  },
+  additionalLinkTags: [
+    {
+      rel: "canonical",
+      href: "https://mahmoud-elbehery.netlify.app/",
+    },
+  ],
+  additionalMetaTags: [
+    {
+      property: "linkedin:site",
+      content: "https://www.linkedin.com/in/mahmoud-ramy-elbehery-752aab235",
+    },
+    {
+      property: "twitter:site",
+      content: "https://x.com/mahmoudramy93",
+    },
+    {
+      property: "author",
+      content: "Mahmoud Elbehery",
+    },
+    {
+      property: "publisher",
+      content: "Mahmoud Elbehery Portfolio",
+    },
+    {
+      property: "keywords",
+      content:
+        "backend development, API design, database schema, cloud deployment, Docker, Ansible, Shell",
+    },
+  ],
+};
+
+export default seoConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
-import type { Metadata } from "next";
 import "./globals.css";
 
 import { Header, SocialMediaFooter } from "@/components/common";
 import Providers from "./Providers";
-
-export const metadata: Metadata = {
-  title: "Mahmoud Elbehery",
-  description:
-    "Explore Mahmoud Elbehery's portfolio. I specialize in backend software development, API design and management, and database schema design (SQL & NoSQL). With expertise in Docker containerization, cloud and on-premise deployment, and Infrastructure as Code using Ansible and Shell, I excel in automation and operations. My experience includes collaboration and CI/CD practices to ensure seamless software delivery.",
-};
+import { DefaultSeo } from "next-seo";
+import seoConfig from "../../next-seo.config";
 
 export default function RootLayout({
   children,
@@ -18,6 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="w-screen h-screen mx-auto overflow-x-hidden bg-black lg:w-5/6">
+        <DefaultSeo {...seoConfig} />
         <Providers defaultTheme="system" attribute="class">
           <Header />
           {children}
